e118e19640024f714057904ef5c5786bf8d84840
Pier-Luc Gendreau <pierluc@outlook.com>
2017-04-01 18:15:57 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const cheerio = require('cheerio')
+const request = require('request-promise')
+const argv = require('yargs').argv
+
+require('console.table')
+
+const { band, album } = argv
+
+if (!band) {
+  throw new Error('missing parameter --band')
+}
+
+if (!album) {
+  throw new Error('missing parameter --album')
+}
+
+(async function ({ band, album }) {
+  function normalize (value) {
+    return value.toLocaleLowerCase().replace(/\s/g, '')
+  }
+
+  const url = `http://www.darklyrics.com/lyrics/${normalize(band)}/${normalize(album)}.html`
+  const ignoreWords = [
+    'and',
+    'are',
+    'for',
+    'have',
+    'his',
+    'our',
+    'the',
+    'they',
+    'was',
+    'will',
+    'with',
+    'you',
+    'your'
+  ]
+
+  console.info(`\nLooking up ${url}\n`)
+  console.time(`${band} - ${album}`)
+
+  const response = await request.get(url)
+  const $ = cheerio.load(response)
+
+  const lyrics = $('div.lyrics').text().toLocaleLowerCase().replace(/\s/g, ' ').replace(/!|\?|,|\./g, '')
+  const wordMap = {}
+
+  lyrics.split(' ').forEach((word) => {
+    wordMap[word] = (wordMap[word] || 0) + 1
+  })
+
+  const words = Object.keys(wordMap).filter((word) => {
+    return word.length > 2 && wordMap[word] > 2 && ignoreWords.indexOf(word) === -1
+  }).map((word, i) => ({
+    word,
+    count: wordMap[word]
+  })).sort((a, b) => b.count - a.count).filter((word, i) => i < 20).map((word, i) => Object.assign({
+    rank: i + 1
+  }, word))
+
+  console.table(words)
+
+  console.timeEnd(`${band} - ${album}`)
+})({ band, album })
---
+++ readme.md
@@ -0,0 +1,52 @@
+# darklyrics
+
+> crawls an album's page and compiles a list of most used words
+
+## installation
+
+```
+npm install darklyrics -g
+```
+
+## usage
+
+### input
+
+```
+darklyrics --band "Twilight Force" --album "Heroes of Mighty Magic"
+```
+
+### output
+
+```
+Looking up http://www.darklyrics.com/lyrics/twilightforce/heroesofmightymagic.html
+
+rank  word     count
+----  -------  -----
+1     over     37
+2     might    37
+3     time     29
+4     ride     28
+5     far      26
+6     magic    25
+7     through  24
+8     light    24
+9     power    23
+10    rise     22
+11    again    22
+12    fly      20
+13    skies    18
+14    mighty   18
+15    dragon   16
+16    hope     15
+17    glory    15
+18    seas     13
+19    bright   13
+20    away     13
+
+Twilight Force - Heroes of Mighty Magic: 336.341ms
+```
+
+## license
+
+MIT
